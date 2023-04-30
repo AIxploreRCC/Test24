85d14f1c2622bb86f53580d5748ed35efe9c3875
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2023-04-30 15:04:54 +0200

    Update ZinoGPT.py

--- ZinoGPT.py
+++ ZinoGPT.py
@@ -3,7 +3,7 @@ import streamlit as st
 from streamlit_chat import message
 import openai
 
-st.set_page_config(page_title="Chat with SimonGPT")
+st.set_page_config(page_title="Chat with ZinoGPT")
 st.title("Chat with SimonGPT")
 st.sidebar.markdown("Developed by Mark Craddock](https://twitter.com/mcraddock)", unsafe_allow_html=True)
 st.sidebar.markdown("Current Version: 0.0.2")
