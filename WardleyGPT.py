3f0a2fc4f1066233b6eba63dd9d01696b64b4ae0
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2023-04-30 14:44:28 +0200

    Update WardleyGPT.py

--- WardleyGPT.py
+++ WardleyGPT.py
@@ -14,7 +14,7 @@ st.sidebar.markdown("May run out of OpenAI credits")
 # Set OpenAI API model
 model = "gpt-4"
 
-openai.api_key = st.secrets('OPENAI_API_KEY')
+openai.api_key = st.secrets("openai.api_key")
 
 def get_initial_message():
     messages=[
